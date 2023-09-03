4987dc99c52cf50ae2e76f54829ce049ad722fe4
rohitladhar <rohit.ladhar10@gmail.com>
2023-09-03 17:53:25 +0530

    second

---
+++ graphql/schema.js
@@ -0,0 +1,24 @@
+const { GraphQLSchema,GraphQLObjectType } = require("graphql")
+
+const {} = require("./queries")
+const {} = require("./mutation")
+
+const QueryType = new GraphQLObjectType({
+    name:'QueryType',
+    description:'Queries',
+    fields:{
+
+    },
+})
+
+const MutationType = new GraphQLObjectType({
+    name:'MutationType',
+    description:'Mutations',
+    fields:{
+
+    }
+})
+module.exports = new GraphQLSchema({
+    query:QueryType,
+    mutation:MutationType
+})
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const  dotenv = require('dotenv')
+const {graphqlHTTP} = require("express-graphql")
+const schema = require("./graphql/schema")
 const app = express()
 const {connectDB} = require('./db') 
 dotenv.config()
@@ -7,6 +9,10 @@ connectDB()
 app.get("/",(req,res)=>{
     res.json({msg:"reached"})
 })
+app.get('/graphql',graphqlHTTP({
+    schema:schema,
+    graphiql:true
+}))
 
 app.listen(process.env.PORT,()=>{
     console.log(`app is listening at ${process.env.PORT}`)
